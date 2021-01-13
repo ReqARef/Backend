7db86266d1d10ca6a2250cbe4c3cf733240bd73f
Kanav Khajuria <kanavkhajuria@Kanavs-MacBook-Pro.local>
2021-01-13 20:48:30 +0530

    Db connection test

---
+++ src/db/database.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'kanav',
+    host: 'localhost',
+    database: 'MoneyManager',
+    password: '554411',
+    port: 5432,
+  })
+
+  module.exports = pool
