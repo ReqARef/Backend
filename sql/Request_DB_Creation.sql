e8ce89b2e4a7a51daa9ff27230dc17086fe9538a
Kanav <khajuriakanav5@gmail.com>
2021-01-17 21:39:06 +0530

    request db in progress

---
+++ sql/Request_DB_Creation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE REQUESTS(
+    id BIGSERIAL PRIMARY KEY,
+    request_from BIGINT REFERENCES USERS(id) NOT NULL,
+    request_to BIGINT REFERENCES USERS(id) NOT NULL,
+    job_id varchar NOT NULL,
+    company_id BIGINT REFERENCES COMPANIES(id) NOT NULL,
+    job_url varchar NOT NULL,
+    job_name varchar NOT NULL,
+    referral_status SMALLINT NOT NULL DEFAULT 0,
+	referral_acknowledge SMALLINT NOT NULL DEFAULT 0,
+	timestamp NOT NULL DEFAULT NOW()
+)
+
+-- create index
--- src/utils/DatabaseConstants.js
+++ src/utils/DatabaseConstants.js
@@ -4,6 +4,13 @@ const REQUESTS_referral_status = {
 	2: 'REJECT'
 };
 
+const REQUESTS_referral_acknowledge = {
+	0: 'PENDING',
+	1: 'SUCCESS',
+	2: 'REJECT'
+};
+
 module.exports = {
-	REQUESTS_referral_status
+	REQUESTS_referral_status,
+	REQUESTS_referral_acknowledge
 };
