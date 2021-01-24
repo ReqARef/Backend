0968bc97c7a6be77935d90f46936f0aa5c36d9fe
Kanav <khajuriakanav5@gmail.com>
2021-01-24 21:49:40 +0530

    code structure changes

--- src/routers/signUpRouter.js
+++ src/routers/signUpRouter.js
@@ -4,9 +4,9 @@ const Pool = require('../db/database');
 const bcrypt = require('bcryptjs');
 
 const validateUserObject = (userObject) => {
-	const { email, fName, lName, password, role, companyName, experience, college, jobRole,
+	const { email, firstName, lastName, password, role, companyName, experience, college, jobRole,
 		resume} = userObject;
-	if(!email || !fName || !lName || !password || !role || !companyName || !experience || 
+	if(!email || !firstName || !lastName || !password || !role || !companyName || !experience || 
 		!college || !jobRole || !resume){
 		return false;
 	}
@@ -14,35 +14,36 @@ const validateUserObject = (userObject) => {
 };
 
 router.post('/signup' , async(req,res) => {
+	const result = {status: false};
 	try{
 		const userObject = req.body;
 		if(!validateUserObject(userObject)){
 			throw new Error('Invalid inputs');
 		}
-		var queryString = 'SELECT COUNT(*) FROM USERS WHERE email= \''+ userObject.email+ '\'';
-		const queryResult = await Pool.query(queryString);
-		if(queryResult.rows[0].count>0){
-			throw new Error('Email is already registered');
+		const checkExistingEmailString = `SELECT id FROM USERS WHERE email=
+			'${userObject.email.toLowerCase()}'`;
+		const checkExistingEmailResult = await Pool.query(checkExistingEmailString);
+		if(checkExistingEmailResult.rows.length > 0){
+			result['error'] = 'Email already exist';
+			return res.send(result);
 		}
 		userObject.password = await bcrypt.hash(
 			userObject.password, parseInt(process.env.bcrypt_rounds)
 		);
-		queryString = `INSERT INTO USERS(email,first_name,last_name,password,role,company_name,
-			experience,college,job_role,resume) VALUES('${userObject.email}', 
+		const insertRowString = `INSERT INTO USERS(email,first_name,last_name,password,role,
+			company_name, experience,college,job_role,resume) VALUES('${userObject.email.toLowerCase()}', 
 			'${userObject.firstName}', '${userObject.lastName}', '${userObject.password}',
 			'${userObject.role}', '${userObject.companyName}', '${userObject.experience}',
 			'${userObject.college}', '${userObject.jobRole}', '${userObject.resume}');`;
-		await Pool.query(queryString);
-		res.send({
-			status: true,
-			message : 'User Added Successfully'
-		});
+		await Pool.query(insertRowString);
+		result['status'] = true;
+		result['message'] = 'User Added Successfully';
+		res.send(result);
 	}
 	catch(err){
-		return res.send({
-			status: false,
-			error : err.message
-		});
+		console.log(err);
+		result.error = err.message;
+		return res.send(result);
 	}
 });
 
