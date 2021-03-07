96fb653df076ffd14bbccd1acccdec341d2a2727
Kanav <khajuriakanav5@gmail.com>
2021-03-07 15:24:20 +0530

    Company search linked with UI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const AuthRouter = require('./routers/Auth');
 const otpRouter = require('./routers/OTPRouter');
 const requestRouter = require('./routers/RequestRouter');
+const companyRouter = require('./routers/Company');
 const port = process.env.port || 3000;
 const cookieParser = require('cookie-parser');
 const app = express();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(AuthRouter);
 app.use(otpRouter);
 app.use(requestRouter);
+app.use(companyRouter);
 
 app.listen(port, () => {
 	console.log('Express up on port:'+port);
