e6801b86a3f90c86c9b6a30c4c7d503aeea6ccdb
rsalwan98 <salwanrohit1998@gmail.com>
2021-07-11 20:22:59 +0530

    accept reject functionality

--- src/utils/RequestHelper.js
+++ src/utils/RequestHelper.js
@@ -1,3 +1,5 @@
+const db = require('../db/database');
+const Pool = db.getPool();
 const checkRequestObjectForNull = (requestObject) => {
     const { requestFrom, requestTo, jobId, companyName, jobUrl, comments } =
         requestObject;
@@ -42,8 +44,27 @@ const convertToBase64 = (temp) => {
     return Buffer.from(temp).toString('base64');
 };
 
+const getRequestHelper = async (userEmail) => {
+    const requestSearchString = `SELECT * FROM REQUESTS WHERE request_to = '${userEmail}';`;
+    const requestSearchResult = await Pool.query(requestSearchString);
+    let requests = [];
+    if (requestSearchResult != null) {
+        requests = requestSearchResult.rows;
+    }
+
+    for (var i = 0; i < requests.length; i++) {
+        const userSearchQuery = `SELECT * FROM USERS WHERE \
+            email='${requests[i].request_from}';`;
+        const userSearchQueryResult = await Pool.query(userSearchQuery);
+        requests[i]['user'] = userSearchQueryResult.rows[0];
+    }
+    console.log(requests);
+    return requests;
+}
+
 module.exports = {
     checkRequestObjectForNull,
     generateId,
-    extractRefreshTokenHeaderFromReq
+    extractRefreshTokenHeaderFromReq,
+    getRequestHelper
 };
