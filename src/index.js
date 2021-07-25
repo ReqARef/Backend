4e0abd08415d9d89301a5c01a3dcdc089ba1211a
Kanav <khajuriakanav5@gmail.com>
2021-07-25 19:04:08 +0530

    log

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const searchRouter = require('./routers/Search');
 const profileRouter = require('./routers/Profile');
 const statsRouter = require('./routers/Stats');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
+console.log('cors enabled for ' + process.env.website_host);
 app.use(cors({ credentials: true, origin: process.env.website_host }));
 app.use(cookieParser());
-
 app.use(AuthRouter);
 app.use(otpRouter);
 app.use(requestRouter);
